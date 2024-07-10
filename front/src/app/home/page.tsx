5498ff3829949285ad4d37afbda843e90923050e
arielZarate <arieltecnico@gmail.com>
2024-07-10 20:41:17 -0300

    mergee los cambios

--- front/src/app/home/page.tsx
+++ front/src/app/home/page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import TestimonialsSection from "@/components/Testimonials/TestimonialsSection";
+import Hero from "@/components/Hero/Hero";
 
 type Props = {};
 
 const HomePage = (props: Props) => {
   return (
     <div className="my-20 min-h-screen min-w-full">
+      <Hero />
       <TestimonialsSection />
     </div>
   );

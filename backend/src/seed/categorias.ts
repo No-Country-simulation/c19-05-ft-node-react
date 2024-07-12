5b0b00a11d5d868a96a499a8f7ab144cc84e788a
CamiloTello002 <camilojtello5@gmail.com>
2024-07-12 12:37:25 -0500

    Se añadieron comentarios que me permiten entender cómo va el código

--- backend/src/seed/categorias.ts
+++ backend/src/seed/categorias.ts
@@ -1,6 +1,7 @@
 import Category from "../models/Category.model";
 import Specialty from "../models/Specialty.model";
 
+// custom id para únicamente identificar cada uno 
 export const categories = [
     {name:"Salud y Bienestar",customId:1},
     {name:"Tecnologia e Informatica",customId:2},
@@ -16,6 +17,7 @@ export const categories = [
   ];
 
   
+  // cada customId va a coincidir con la customId de las categorías que ya existen
 export const specialties = [
     { name: "Nutricion", categoryId:"",customId:1},
     { name: "Fisioterapia", categoryId:"" ,customId:1},
@@ -52,10 +54,16 @@ export const specialties = [
 export const addCategories = async (categories:{name:string,customId:number}[]) => {
 
     try {
+        // toma el array de categorías, donde cada uno tiene su nombre y id propio
         const addArray = categories.map(category => {
+            // Y cada elemento del array lo convierte en una promesa,
+            // la cual una vez cumplida, los agrega a la base de datos
             return Category.create(category)
         })
         
+        // Y esta línea agrega todos los elementos a la base de datos
+        // Vale la pena resaltar que MongoDB se encargará de darle un identificador único
+        // a cada categoría, y será útil para asignarle el id de categoría a cada especialidad
         await Promise.allSettled(addArray);
         console.log(addArray);
         return "Carga completa"
@@ -68,15 +76,25 @@ export const addCategories = async (categories:{name:string,customId:number}[]) 
 
 export const addSpecialties = async (specialties:{name:string,categoryId:string,customId:number}[]) => {
     try {
+        // Consigue todas las categorías que se han agregado a la base de datos
         const categories = await Category.find();  
+        // Luego, toma todas las especialidades e itera sobre todas ellas.
         const newArray = specialties.map(specialty => {
+            // Recorre todo el array de categorías que se sacó de la base de datos.
+            // también toma el índice
             for (let index = 0; index < categories.length; index++) {
+                // Si el customId de la especialidad que viene del array de especialidades
+                // coincide con el customId de cada categoría que se está recorriendo, 
                 if(specialty.customId === categories[index].customId) {
+                // entonces se le agrega un campo llamado categoryId, el cual es el mismo
+                // id que tiene la categoría que MongoDB le añadió por defecto
                     specialty.categoryId = categories[index].id
+                    // Y se crea el array de promesas
                     return Specialty.create(specialty)
                 }               
             }
         })
+        // que una vez ejecutados, agregarán las especialidades a la base de datos
         await Promise.allSettled(newArray)
         console.log(newArray);  
         return "Seed completo"

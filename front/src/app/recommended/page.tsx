c4b89ce304fd19d1c3632a0cd2e2222fa59bc541
CamiloTello002 <camilojtello5@gmail.com>
2024-07-24 11:23:07 -0500

    feat(route): Add route for recommended users

---
+++ front/src/app/recommended/page.tsx
@@ -0,0 +1,12 @@
+import About from '@/components/About/About';
+
+type Props = {};
+
+const AboutPage = (props: Props) => {
+  return (
+    <div className="my-20 min-h-screen min-w-full">
+      <About />
+    </div>
+  );
+};
+export default AboutPage;

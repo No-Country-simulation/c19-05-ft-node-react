880963f53bfff4e82bf050bc8d47bc4592d63b4e
CamiloTello002 <camilojtello5@gmail.com>
2024-07-29 14:50:37 -0500

    chore(user seed): Remove comments and add profile photos

--- backend/src/routes/Seed.routes.ts
+++ backend/src/routes/Seed.routes.ts
@@ -36,28 +36,21 @@ routerSeed.get("/seed/specialties", async (req: Request, res: Response) => {
 
 routerSeed.get("/seed/users", async (req: Request, res: Response) => {
   try {
-    // 1) conseguir lista de especialidades, las cuales vamos a usar para agregar a cada usuario
     const specialties = await Specialty.find();
-    // 2) filtrar la lista
     const specialtiesFiltered: specialty[] = specialties.map((specialty) => {
-      // Extrae los dos campos que nos interesan
       const { _id, categoryId } = specialty;
-      // para cada elemento, devuelve un objeto con los campos deseados
       return {
         specialtyId: _id,
         categoryId,
       } as specialty;
     });
-    // 3) tomar los usuarios y hashear sus contraseñas
     let usersHashed: IUserSeed[] = await hashAllPasswords(users);
 
-    // 4) agregar especialidades a cada usuario
     usersHashed = assignSpecialtiesAndInterests(
       usersHashed,
       specialtiesFiltered
     );
 
-    // 5) después de haber agregado las especialidades, se añaden a la base de datos
     const user = await UserModel.create(usersHashed);
 
     res.status(201).json({
